73b75dfbb4e19c660b2ad1e5adf9b62ff7fb9ad1
Samer Buna <samer.buna@gmail.com>
2015-12-05 14:03:29 -0800

    Cleanup

--- public/GoogleLogo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class GoogleLogo extends  React.Component {
-  render() {
-    return <img className="img-responsive" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />;
-  }
-}
-
-export default GoogleLogo;
--- public/script.js
+++ public/script.js
@@ -1,34 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import ReasonsWhyWeLikeReact from "./ReasonsWhyWeLikeReact";
-
-let pplToGreet = ["Nick", "Cade", "Joe"];
-
-class Greet extends React.Component {
-  render() {
-    return (
-      <div className="green">
-        {this.props.greeting} {this.props.children}
-      </div>
-    );
-  }
-}
-
 class App extends React.Component {
   render() {
     return (
-      <div>
-
-        <Greet greeting="Hi">
-          <b>Nick</b>
-        </Greet>
-
-        <Greet greeting="Howdy">
-          Cade
-        </Greet>
-
-      </div>
+      <div>Change Me</div>
     );
   }
 }
